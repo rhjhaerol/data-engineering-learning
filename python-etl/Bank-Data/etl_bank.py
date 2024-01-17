8659ec14012dd01464fa5d0cd6269c5fd8009244
rhjhaerol <rhjhaerol@gmail.com>
2024-01-17 10:56:53 +0700

    transform and load data

--- python-etl/Bank-Data/etl_bank.py
+++ python-etl/Bank-Data/etl_bank.py
@@ -11,7 +11,11 @@ from datetime import datetime
 
 url = 'https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks'
 table_attribs = ["Name", "MC_USD_Billion"]
-csv_path = './Bank_MC.csv'
+output_path = './Largest_banks_data.csv'
+csv_path = './exchange_rate.csv'
+table_name = 'Largest_banks'
+db_name = 'Largest_banks'
+
 
 def log_progress(message):
     ''' This function logs the mentioned message of a given stage of the
@@ -47,6 +51,13 @@ def transform(df, csv_path):
     information, and adds three columns to the data frame, each
     containing the transformed version of Market Cap column to
     respective currencies'''
+    data = pd.read_csv(csv_path)
+
+    exchange_rate = data.set_index('Currency').to_dict()['Rate']
+
+    df['MC_EUR_Billion'] = [np.round(float(x)*exchange_rate['EUR'],2) for x in df['MC_USD_Billion']]
+    df['MC_GBP_Billion'] = [np.round(float(x)*exchange_rate['GBP'],2) for x in df['MC_USD_Billion']]
+    df['MC_INR_Billion'] = [np.round(float(x)*exchange_rate['INR'],2) for x in df['MC_USD_Billion']]
 
     return df
 
@@ -58,10 +69,15 @@ def load_to_csv(df, output_path):
 def load_to_db(df, sql_connection, table_name):
     ''' This function saves the final data frame to a database
     table with the provided name. Function returns nothing.'''
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
+
 
 def run_query(query_statement, sql_connection):
     ''' This function runs the query on the database table and
     prints the output on the terminal. Function returns nothing. '''
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
 
 ''' Here, you define the required entities and call the relevant
 functions in the correct order to complete the project. Note that this
@@ -73,6 +89,30 @@ df = extract(url, table_attribs)
 
 log_progress('Data extraction complete. Initiating Transformation process')
 
-load_to_csv(df, csv_path)
+df = transform(df, csv_path)
+
+log_progress('Data transformation complete. Initiating loading process')
+
+load_to_csv(df, output_path)
 
 log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect(db_name)
+
+log_progress('SQL Connection initiated.')
+
+load_to_db(df, sql_connection, table_name)
+
+log_progress('Data loaded to Database as table. Running the query')
+
+query_statement1 = f"SELECT * from {table_name}"
+query_statement2 = f"SELECT AVG(MC_GBP_Billion) FROM {table_name}"
+query_statement3 = f"SELECT Name from {table_name} LIMIT 5"
+
+run_query(query_statement1, sql_connection)
+run_query(query_statement2, sql_connection)
+run_query(query_statement3, sql_connection)
+
+log_progress('Process Complete.')
+
+sql_connection.close()
